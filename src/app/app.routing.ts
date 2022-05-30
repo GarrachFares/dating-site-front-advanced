d81f163a917dc7158f65e67cff97cfbccfad09d7
Garrach <faresgarrach@insat.u-carthage.tn>
2022-05-30 17:31:48 +0100

    fixing logout , login and guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,6 +13,7 @@ import { ProfilComponent } from "./core/pages/profil/profil.component";
 import {RoomComponent} from "./core/pages/messaging/room/room.component";
 import {ChoiceComponent} from "./core/pages/choice/choice.component";
 import { CategoriesComponent } from "./core/pages/categories/categories.component";
+import { AuthGuard } from "./core/guards/auth.guard";
 
 
 
@@ -25,9 +26,9 @@ const APP_ROUTING: Routes=[
     {path:'explore',component:ExploreComponent,children:[
         {path:'',component:DashboardComponent},
         {path:'profil',component:ProfilComponent}
-    ]},
+    ], canActivate: [AuthGuard]},
     {path:'messaging',component:MessagingComponent},
-    {path:'room',component:RoomComponent},
+    {path:'room',component:RoomComponent, canActivate: [AuthGuard]},
     {path:'choice',component:ChoiceComponent},
     {path:'categories',component:CategoriesComponent}
     //{path:'sign-in',component:SignInComponent}
